c7118ae8eb05137e2c4e45964ec71f39d7946a74
JohnWest <johnwest@umich.edu>
2020-09-14 03:56:55 -0400

    Initial Commit

---
+++ RiddlerSim3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RiddlerSim3
+//
+//  Created by John West on 9/14/20.
+//  Copyright © 2020 John West. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
